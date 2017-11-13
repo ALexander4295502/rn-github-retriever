96a76d770c2b90a02bd4367db4a47fec74d0737d
ALexander4295502 <yuanzhengstl@gmail.com>
2017-11-13 16:36:49 -0600

    add profile page for userInfo

--- App/Components/Dashboard.js
+++ App/Components/Dashboard.js
@@ -2,8 +2,11 @@ import React, {Component} from 'react';
 import {
   Text,
   View,
-  StyleSheet
+  Image,
+  StyleSheet,
+  TouchableHighlight
 } from 'react-native';
+import Profile from "./Profile";
 
 const styles = StyleSheet.create({
   container: {
@@ -21,11 +24,57 @@ const styles = StyleSheet.create({
 });
 
 export default class Dashboard extends Component {
+  makeBackground(btn){
+    var obj = {
+      flexDirection: 'row',
+      alignSelf: 'stretch',
+      justifyContent: 'center',
+      flex: 1
+    };
+    if(btn == 0) {
+      obj.backgroundColor = '#48BBEC';
+    } else if(btn == 1){
+      obj.backgroundColor = '#E77AAE';
+    } else {
+      obj.backgroundColor = '#758BF4';
+    }
+    return obj;
+  }
+  goToProfile() {
+    this.props.navigator.push({
+      title: 'Profile Page',
+      component: Profile,
+      passProps: {userInfo: this.props.userInfo}
+    });
+  }
+  goToRepos() {
+    console.log('Going to Repos Page');
+  }
+  goToNotes() {
+    console.log('Going to Notes Page');
+  }
   render(){
     return (
       <View style={styles.container}>
-        <Text>This is the dashboard</Text>
-        <Text> the blog is {this.props.userInfo.blog} </Text>
+        <Image source={{url: this.props.userInfo.avatar_url}} style={styles.image}/>
+        <TouchableHighlight
+          onPress={this.goToProfile.bind(this)}
+          style={this.makeBackground(0)}
+        >
+          <Text style={styles.buttonText}>View Profile </Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          onPress={this.goToRepos.bind(this)}
+          style={this.makeBackground(1)}
+        >
+          <Text style={styles.buttonText}>View Repos </Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          onPress={this.goToNotes.bind(this)}
+          style={this.makeBackground(2)}
+        >
+          <Text style={styles.buttonText}>View Notes </Text>
+        </TouchableHighlight>
       </View>
     )
   }
